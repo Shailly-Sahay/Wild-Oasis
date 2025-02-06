0281b61c47d43d81edcd373d6270a5128f202cd2
Shailly-Sahay <shaillysahayy@gmail.com>
2025-02-06 15:25:46 +0530

    feat: render cabin form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,24 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import { Button } from "../ui";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>TEST</p>
       </Row>
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm(!showForm)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
